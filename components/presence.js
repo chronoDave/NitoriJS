a17c5b0df3a8814cce94549317a5c131aa8e9593
Chronocide <davidwolters6758@gmail.com>
2018-08-28 15:45:49 +0200

    Converted admin plugin into component
    Features permissions, server, prefix and self commands

--- components/presence.js
+++ components/presence.js
@@ -2,18 +2,68 @@ const Presence = require('./presence.json');
 const presenceList = Presence.list;
 
 module.exports = {
-	randomPresence: function(bot) {
+	random: function(bot) {
 		let random = Math.floor(Math.random() * presenceList.length);
-		let randomStatus = presenceList[random].status; 
+		let randomName = presenceList[random].status; 
 		let randomType = presenceList[random].type;
 
-		bot.user.setPresence({
-			game: {
-				name: randomStatus,
-				type: randomType
+		try {
+			bot.user.setPresence({
+				game: {
+					name: randomName,
+					type: randomType
+				}
+			});
+			if (randomType == "listening") {
+				return `Let's see... I'll be ${randomType} to ${randomName}, then!`;
+			} else {
+				return `Let's see... I'll be ${randomType} ${randomName}, then!`
 			}
-		});
-
-		return console.log(` - Presence set to: ${randomType} ${randomStatus}`);
+		}
+		catch (e) {
+			return `Contact Chrono and send him this: ${e.stack}`;
+		}
+	},
+	watching: function(status, bot) {
+		try {
+			bot.user.setPresence({
+				game: {
+					name: status,
+					type: "WATCHING"
+				}
+			});
+			return `Alright, I'll be watching ${status}!`;
+		}
+		catch (e) {
+			return `Contact Chrono and send him this: ${e.stack}`;
+		}
+	},
+	listening: function(status, bot) {
+		try {
+			bot.user.setPresence({
+				game: {
+					name: status,
+					type: "LISTENING"
+				}
+			});
+			return `Alright, I'll be listening to ${status}!`;
+		}
+		catch (e) {
+			return `Contact Chrono and send him this: ${e.stack}`;
+		}
+	},
+	playing: function(status, bot) {
+		try {
+			bot.user.setPresence({
+				game: {
+					name: status,
+					type: "PLAYING"
+				}
+			});
+			return `Alright, I'll be playing ${status}!`;
+		}
+		catch (e) {
+			return `Contact Chrono and send him this: ${e.stack}`;
+		}
 	}
 }
