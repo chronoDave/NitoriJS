f320ad2c72d884830cdb42f58a5e532d61f72b37
Chronocide <davidwolters6758@gmail.com>
2018-08-28 15:54:28 +0200

    Revert "Converted admin plugin into component"
    This reverts commit a17c5b0df3a8814cce94549317a5c131aa8e9593.

--- components/admin.js
+++ components/admin.js
@@ -1,5 +1,4 @@
 const Sequelize = require('sequelize');
-const Presence = require('./presence.js');
 
 const sequelize = new Sequelize('database', 'user', 'password', {
 	host: 'localhost',
@@ -13,155 +12,37 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 const Permissions = sequelize.define('permissions', {
 	id: {
 		type: Sequelize.INTEGER,
-		allowNull: false,
-		primaryKey: true,
-		unique: true
+		unique: true,
+		primaryKey: true
 	},
 	name: Sequelize.STRING,
+	guild: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	}
 });
 
-const Prefix = sequelize.define('prefix', {
-	id: {
-		type: Sequelize.INTEGER,
-		allowNull: false,
-		primaryKey: true,
-		unique: true
-	},
-	name: Sequelize.STRING,
-	prefix: Sequelize.STRING
-})
-
 module.exports = {
-	// Admin user functions
 	create: function(debug) {
-		if (!debug) {
-			Permissions.sync();
-			Prefix.sync();
-		} else {
-			Permissions.sync({force: true});
-			Prefix.sync({force: true});
-		}
+		if (!debug) return Permissions.sync();
+		return Permissions.sync({force: true});
 	},
-	add: async function(user, id) {
+	add: async function(user, id, message) {
 		try {
 			await Permissions.create({
 				id: user.id,
-				name: user.username + '#' + user.discriminator
+				name: user.username,
+				guild: id
 			});
-			return `User ${user.username} added.`;
+			return message.reply(`User ${user.username} added.`);
 		}
 		catch (e) {
-			if (e.name = "SequelizeUniqueConstraintError") return `${user.username + '#' + user.discriminator} already exists`;
-			return 'If you see this, contact Chronocide';
+			if (e.name = "SequelizeUniqueConstraintError") return message.reply(`${user.username} already exists`);
+			return message.reply('If you see this, contact Chronocide');
 		}
 	},
-	list: async function() {
-		let list = await Permissions.findAll({attributes: ['name']});
-		if (list.length == 0) return "There are no users in the database";
+	get: async function(id) {
+		let list = await Permissions.findAll({where: {guild: id}, attributes: ['name']});
 		return list.map(t => t.dataValues.name).join(', ')
-	},
-	delete: async function(user) {
-		let deletedEntries = await Permissions.destroy({where: {id: user.id}});
-		if (!deletedEntries) return `That user didn't exist`;
-		return 'User deleted';
-	},
-	check: async function(user) {
-		let checkedUser = await Permissions.findOne({where: user.id});
-		if (!checkedUser) return false;
-		return true;
-	},
-	// Prefix functions
-	prefix: async function(channel, command, prefix) {
-		switch (command) {
-			case `set`:
-				try {
-					await Prefix.create({
-						id: channel.id,
-						name: channel.name,
-						prefix: prefix
-					});
-					return `Prefix for this server set to ${prefix}`;
-				}
-				catch (e) {
-					if (e.name = "SequelizeUniqueConstraintError") {
-						let changedPrefix = await Prefix.update({prefix: prefix}, {where: {id: channel.id}});
-						if (changedPrefix > 0) return `Prefix set to ${prefix}`;
-						return `Could not find a custom prefix for this server, perhaps it's not set yet?`;
-					}
-					return 'If you see this, contact Chronocide';
-				}
-			case 'get':
-				let checkedPrefix = await Prefix.findOne({where: {id: channel.id}});
-				if (checkedPrefix) return checkedPrefix.prefix;
-				return false;
-			default:
-				return '`' + `${command}` + '`' + ` is not a valid command`;
-		}
-	},
-	// Server functions
-	server: function(command, id, bot) {
-		switch (command) {
-			case 'list':
-				let arrayMessage = [];
-				for (var [key, value] of bot.guilds) {
-					arrayMessage.push(`${key} - ${bot.guilds.get(key).name}`);
-				}
-				return arrayMessage;
-			case 'leave':
-				try {
-					bot.guilds.get(id).leave();
-					return `I've left` + '`' + `${bot.guilds.get(id).name}` + '`';
-				}
-				catch (err) {
-					return `I can't leave that server, did you get the ID wrong?`;
-				}
-			default:
-				return '`' + `${command}` + '`' + ` is not a valid command`;
-		}
-	},
-	// Self functions
-	self: function(command, arg, status, bot) {
-		switch (command) {
-			case 'status':
-				if (status.includes('<@')) return `I can't process highlights, please type the user's name instead!`;
-				switch (arg) {
-					case 'random':
-						return Presence.random(bot);
-					case 'watching':
-						return Presence.watching(status, bot)
-					case 'listening':
-						return Presence.listening(status, bot)
-					case 'playing':
-						return Presence.playing(status, bot)
-					case undefined:
-						return 'What status do you have in mind?';
-					default:
-						return `That's not a valid status`;
-				}
-			case 'avatar':
-				if (arg == undefined) return 'What avatar do you have in mind?';
-				if (arg.match(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/g)) {
-					try {
-						bot.user.setAvatar(arg);
-						return 'New avatar set!';
-					}
-					catch(e) {
-						return `Contact Chrono and send him this: ${e.stack}`;
-					}
-				} else {
-					return `I can't use that format, please provide a .png, .gif or .jpg image.`;
-				}
-			case 'name':
-				if (arg == undefined) return 'What name do you have in mind?';
-				try {
-					bot.user.setUsername(arg.match());
-					return `I have renamed myself to ${arg}!`;
-				}
-				catch (e) {
-					return `Contact Chrono and send him this: ${e.stack}`;
-				}
-			default:
-				return '`' + `${command}` + '`' + ` is not a valid command`;
-		}
 	}
 }
--- main.js
+++ main.js
@@ -11,7 +11,7 @@ Nitori.plugins = new Discord.Collection();
 const NitoriClever = new Cleverbot;
 NitoriClever.configure({botapi: "CC8k2Ti6DbbtFUm68Wpurk3JMXw"});
 
-var prefix = Settings.prefix; // Can be changed by admin component
+const prefix = Settings.prefix;
 
 // ------------- Components ------------- //
 const Presence = require('./components/presence.js');
@@ -45,7 +45,7 @@ Nitori.on('ready', async () => {
 		console.log("\u001b[31m", e.stack, "\x1b[0m");
 	}
 	// Components
-	console.log(Presence.random(Nitori));
+	Presence.randomPresence(Nitori);
 	Admin.create();
 	console.log('\x1b[32m%s\x1b[0m', 'Discord successfully initialized, Nitori ready for duty!');
 });
@@ -73,55 +73,31 @@ Nitori.on('message', async message => {
 		});
 	}
 	// Listen to commands from here
-	Admin.prefix(message.channel, "get").then(response => {
-		if (response != false) prefix = response; // Check for custom prefixes	
-		if(!command.startsWith(prefix)) return; // Ignore non-commands
-		let noPrefix = command.slice(prefix.length);
-
-		switch (noPrefix) {
-			case 'help':
-				let embed = new Discord.RichEmbed();
-				Help.get(Nitori, message, args, embed);
-				break;
-			case 'admin':
-				/* 
-					Only available for bot owner or users in the database
-					Oh, and the owner is omnipotent, of course
-				*/
-				if (message.channel.type != "text") return message.reply(`My permission only work in servers, not DM's`); // Only works in servers
-				Admin.check(message.author.id).then(response => {
-					if (!response && message.author.id != Settings.owner) return message.reply('Sorry, you have no permission');
-					let user = message.mentions.users.array(); // Ignore other mentions for now
-
-					switch (args[0]) {
-						case 'add':
-							if (user.length == 0) return `Please use @user to select people`;
-							if (user.length !== 1) return `I'm sorry, but I can only add one user at a time`;
-							return Admin.add(user[0]).then(response => {message.reply(response)});
-						case 'list':
-							return Admin.list().then(response => {
-								message.reply(`Here's the list of admins: ${response}`);
-							});
-						case 'delete':
-							if (user.length == 0) return `Please use @user to select people`;
-							if (user.length !== 1) return message.reply(`I can only process a single user at a time!`);
-							if (user[0].id == message.author.id) return message.reply(`You can't delete yourself from the database, silly`);
-							return Admin.delete(user[0]).then(response => {message.reply(response)});
-						case 'prefix':
-							return Admin.prefix(message.channel, args[1], args[2]).then(response => {message.reply(response)});
-						case 'server':
-							return message.reply(Admin.server(args[1], args[2], Nitori));
-						case 'self':
-							return message.reply(Admin.self(args[1], args[2], args.slice(3, args.length).join(" "), Nitori));
-						default:
-							return message.reply('`' + args[0] + '`' + ` is not a valid command`);
-					}
-				});
-				break;
-		}
-		let plugin = Nitori.plugins.get(noPrefix);
-		if (plugin) plugin.run(Nitori, message, args); // Valid plugin? Run it.
-	});
+	if(!command.startsWith(prefix)) return; // Ignore non-commands
+	let noPrefix = command.slice(prefix.length);
+	switch (noPrefix) {
+		case 'help':
+			let embed = new Discord.RichEmbed();
+			Help.get(Nitori, message, args, embed);
+			break;
+		case 'admin':
+			switch (args[0]) {
+				case 'add':
+					let user = message.mentions.users.array();
+					// Add proper error handling
+					if (user.length !== 1 || message.guild == "null") return message.reply(`I'm sorry, but I can only add one user at a time`);
+					Admin.add(user[0], message.guild.id, message);
+					break;
+				case 'get':
+					Admin.get(message.guild.id).then(response => {message.reply(response)});
+					break;
+				default:
+					return message.reply('`' + args[0] + '`' + ` is not a valid command`);
+			}
+			break;
+	}
+	let plugin = Nitori.plugins.get(noPrefix);
+	if (plugin) plugin.run(Nitori, message, args); // Valid plugin? Run it.
 });
 
 // ------------- Discord Login ------------- //
